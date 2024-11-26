b7bbb9871cb85cabf15984869180413184738c69
amrutwali <amrutwali@gmail.com>
2024-11-26 16:16:47 +0530

    [CH02]: Create boylerplet

---
+++ src/challenge-02-cube-conundrum/part-01/index.ts
@@ -0,0 +1,4 @@
+export default async function part01(): Promise<number> {
+    const result = 0;
+    return result;
+}
---
+++ src/challenge-02-cube-conundrum/part-02/index.ts
@@ -0,0 +1,4 @@
+export default async function part02(): Promise<number> {
+    const result = 0;
+    return result;
+}
