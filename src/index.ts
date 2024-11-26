7cfe68e8bf92b5c3c003be9622c0c6add1d4d021
amrutwali <amrutwali@gmail.com>
2024-11-26 16:20:39 +0530

    [MAIN]: Update the resolver to handle CLI args

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,46 @@
 // TODO: import day wise problems
-import challenge01 from "./challenge-01-terbuchet/index.js";
+import challenge01 from "./challenge-01-trebuchet/index.js";
+import challenge02 from "./challenge-02-cube-conundrum/index.js";
 
 // TODO: run and evaluate them one by one
-const evaluator = async () => {
-    // Challenge 01
-    console.info(`[START]: Challenge 01`);
-    const resultChallenge01 = await challenge01();
-    console.info(`\n[SOLUTION 01]: ${JSON.stringify(resultChallenge01)}\n`);
-    console.info(`[END]`);
+const evaluator: (challengeNumber: number) => void = async (
+    challengeNumber
+) => {
+    switch (challengeNumber) {
+        case 1: {
+            // Challenge 01
+            console.info(`[START]: Challenge 01`);
+            const resultChallenge01 = await challenge01();
+            console.info(
+                `\n[SOLUTION 01]: ${JSON.stringify(resultChallenge01)}\n`
+            );
+            console.info(`[END]`);
+            return;
+        }
+        case 2: {
+            // Challenge 02
+            console.info(`[START]: Challenge 02`);
+            const resultChallenge01 = await challenge02();
+            console.info(
+                `\n[SOLUTION 02]: ${JSON.stringify(resultChallenge01)}\n`
+            );
+            console.info(`[END]`);
+            return;
+        }
+    }
 };
 
-evaluator();
+// Get command Line args
+const args = process.argv.slice(2);
+
+// Validate if the program number is passed as an argument
+if (!args.length) {
+    throw new Error(
+        "Argument <program number> not passed. Please add and argument and run again"
+    );
+} else {
+    // Convert argument to integer
+    const programNumber: number = parseInt(args[0]);
+    // Run main evaluator
+    evaluator(programNumber);
+}
